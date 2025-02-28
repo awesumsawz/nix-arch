f3562862481f4584cdb229716e67dc9841e9af46
Jason Biggs <jason@thinkbigg.dev>
2025-02-28 04:21:33 -0600

    updates

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,11 +15,12 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-     pkgs = nixpkgs.legacyPackages.${system};
+      # pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { system = system; };
     in {
       homeConfigurations."rogue" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
+        
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ 
@@ -29,10 +32,9 @@
           #     };
           #   };
           # }
+
         ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
